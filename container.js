939249491a9b6053b92938cf1c37106876ede577
Valentín Romero <valentin_romero_22@hotmail.com>
2022-09-04 21:48:08 -0300

    server y container

--- container.js
+++ container.js
@@ -1,21 +1,73 @@
-const { options } = require("./options/mdb.js")
-const knex = require("knex")(options)
+class Container {
+    constructor(options, table) {
+        this.table = table
+        this.knex = require("knex")(options)
 
-knex.schema
-    .createTable('chat', (table) =>{
-        table.increments('id'),
-        table.string('user'),
-        table.string('message')
-        table.datetime('datetime')
-    })
-    .then(() =>{
-        console.log('okazo')
-    })
-    .catch((error) =>{
-        console.log(error)
-        throw new Error(error)
-    })
-    .finally(() =>{
-        console.log('finally')
-        knex.destroy()
-    })
+        knex.schema
+            .createTableIfNotExists(table, (table) => {
+                table.increments('id'),
+                    table.string('user'),
+                    table.string('message')
+                table.datetime('datetime')
+            })
+            .then(() => {
+                console.log(`Se creó la tabla ${table}`)
+            })
+            .catch((error) => {
+                console.log(error)
+                throw new Error(error)
+            })
+            .finally(() => {
+                console.log('finally')
+                knex.destroy()
+            })
+    }
+
+    save(object) {
+        const result = this.knex(this.table)
+            .insert(object)
+            .then((res) => res)
+            .catch((error) => console.log(error))
+        
+        return result
+    }
+
+    getById(id){
+        const data = this.knex(this.table)
+            .where('id', id)
+            .then((res) => res)
+            .catch((error) => console.log(error))
+
+        return data
+    }
+
+    getAll(){
+        const data = this.knex(this.table)
+            .select('*')
+            .then((res) => res)
+            .catch((error) => console.log(error))
+
+        return data
+    }
+
+    deleteById(id){
+        const result = this.knex(this.table)
+            .where('id', id)
+            .del()
+            .then((res) => res)
+            .catch((error) => console.log(error))
+
+        return result
+    }
+
+    deleteAll(){
+        const result = this.knex(this.table)
+            .del()
+            .then((res) => res)
+            .catch((error) => console.log(error))
+
+        return result
+    }
+}
+
+module.exports = Container
--- server.js
+++ server.js
@@ -1,24 +1,29 @@
-const Contenedor = require("./contenedor")
+const Container = require("./container")
+
+const { options: SQLite } = require("./options/sqlite")
+const { options: MySql } = require("./options/mdb")
+
 const express = require('express')
-const { Router } = express
-const { engine } = require('express-handlebars')
-
-const container = new Contenedor("productos")
-
 const app = express()
 const router = Router()
+const { Router } = express
+
+//const container = new Contenedor("productos")
+const chatContainer = new Container(SQLite, 'chat')
+const productContainer = new Container(MySql, 'product')
 
 const httpServer = require("http").createServer(app)
 const io = require("socket.io")(httpServer)
 
 httpServer.listen(process.env.PORT || 8080, () => console.log('Servidor iniciado...'))
 
+const { engine } = require('express-handlebars')
+app.set('view engine', 'hbs')
+app.set('views', './views')
+
 app.use(express.urlencoded({ extended: true }))
 app.use('/api/products', router)
 app.use('/public', express.static(__dirname + '/public'))
-
-app.set('view engine', 'hbs')
-app.set('views', './views')
 
 app.engine(
     'hbs',
@@ -30,8 +35,8 @@ app.engine(
     })
 )
 
-let chat = []
-let products = []
+/* let chat = []
+let products = [] */
 
 async function getProducts(){
     await container.getAll().then((response) =>{
@@ -44,32 +49,49 @@ async function getProducts(){
     await container.save(product)
 } */
 
-getProducts()
+//getProducts()
 
 router.get('/', (req, res) =>{
     res.render('products', { root: __dirname + '/public' })
 })
 
-io.on('connection', (socket) =>{
-    chat.push('Se unió al chat ' + socket.id)
+io.on('connection', async(socket) =>{
+    const products = await productContainer.getAll()
+    const chat = await chatContainer.getAll()
+    
+    //chat.push('Se unió al chat ' + socket.id)
     io.sockets.emit('chatList', chat)
     io.sockets.emit('productList', products)
 
-    socket.on('chatData', (data) =>{
+    socket.on('chatData', async(data) =>{
+        const added = await chatContainer.save(data)
+        const chat = await chatContainer.getAll()
+
+        added ?
+            io.sockets.emit('chatList', chat):
+            console.log('Error al agregar el chat')
+
+
         chat.push(data)
-        io.sockets.emit('chatList', chat)
     })
 
-    socket.on('productData', (data) =>{        
+    socket.on('productData', async(data) =>{     
+        const added = await productContainer.save(data)
+        const products = await productContainer.getAll()
+
+        added ?
+            io.sockets.emit('productList', products) :
+            console.log('Error al agregar el producto')
+        
+        /* verificar el tema de los id
         const index = products.map(i => i.id).sort((a, b) =>{
             return a - b
-        })
-
+        }) 
         const id = index[index.length - 1] + 1
-        const product = { id, ...data }
+        const product = { id, ...data }*/
+
 
         //saveProduct(product)
-        products.push(product)
-        io.sockets.emit('productList', products)
+        //products.push(product)
     })
 })
