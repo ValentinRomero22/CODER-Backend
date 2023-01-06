b665f6215af4caf2c16a2f51a41c5bec90871fab
Valentín Romero <valentin_romero_22@hotmail.com>
2023-01-06 12:46:41 -0300

    cambios .env

--- config/config.js
+++ config/config.js
@@ -1,15 +1,9 @@
-//const parseArgs = require('minimist')
 const dotenv = require('dotenv')
 const path = require('path')
 
 dotenv.config({
     path: path.resolve(process.cwd(), process.env.NODE_ENV + '.env')
 })
-
-/* const options = { default: { PORT: 8080, MODE: 'FORK' }, alias: { p: 'PORT', m: 'MODE' } }
-const args = parseArgs(process.argv.slice(2), options)
-const PORT = process.env.PORT || 8080
-const MODE = args.MODE */
 
 const PORT = process.env.PORT || 8080
 const MONGO_CONNECTION = process.env.MONGO_CONNECTION
